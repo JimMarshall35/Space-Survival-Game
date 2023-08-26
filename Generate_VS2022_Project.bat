bfdb6eca2ecbcd7bada247ca0473b5ba2ca7c6d2
thearbator <z_raeburn@hotmail.com>
2023-08-26 14:48:31 +0100

    Update build files

--- Generate_VS2022_Project.bat
+++ Generate_VS2022_Project.bat
@@ -4,3 +4,4 @@ for %%I in (.) do set CurrDirName=%%~nxI
 cmake -G "Visual Studio 17 2022" -S "vendor\glfw" -B "vendor\glfw\build"
 call premake\premake5.exe vs2022 %CurrDirName%
 rmdir /s /q x64
+pause
