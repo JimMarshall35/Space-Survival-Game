a203f5a36ccb461866971a8c27f5ba38dcf8b240
Cameron Bell <cameron.bell33@live.co.uk>
2023-08-26 14:45:03 +0100

    fixed 2019 .bat

--- Generate_VS2019_Project.bat
+++ Generate_VS2019_Project.bat
@@ -1,6 +1,6 @@
 @echo off
 call Clean.bat
 for %%I in (.) do set CurrDirName=%%~nxI
-cmake -G "Visual Studio 17 2019" -S "vendor\glfw" -B "vendor\glfw\BUILD"
+cmake -G "Visual Studio 16 2019" -S "vendor\glfw" -B "vendor\glfw\BUILD"
 call premake\premake5.exe vs2019 %CurrDirName%
 rmdir /s /q x64
